85092a86c42c0dfad57e2c0b747ecb5896389272
Alia Morozova <anunna@lilitusec.net>
2023-01-20 21:45:35 +0200

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Create the directory if it doesn't exist
+mkdir -p _wiki/tools
+
 # Run the pacman command and save the output to a variable
 pkgs=$(pacman -Sgg | grep blackarch)
+
+# Create the tools.md file if it doesn't exist
+touch _wiki/tools.md
+echo "# BlackArch Packages" >> _wiki/tools.md
 
 # Loop through the packages
 while read -r pkgname pkgver pkgdesc groups url; do
@@ -17,4 +24,7 @@ while read -r pkgname pkgver pkgdesc groups url; do
   echo "$groups" >> _wiki/tools/$pkgname.md
   echo "## URL" >> _wiki/tools/$pkgname.md
   echo "$url" >> _wiki/tools/$pkgname.md
+  
+  # Add package name and link to package markdown file in tools.md
+  echo "- [$pkgname](tools/$pkgname.md)" >> _wiki/tools.md
 done <<< "$pkgs"
