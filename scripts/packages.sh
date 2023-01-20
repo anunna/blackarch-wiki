53d2be9a7dfe42560fc6ea8b4a5148cce9fa1528
anunna <anunna@lilitusec.net>
2023-01-21 02:41:48 +0300

    updated package script

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Create the directory if it doesn't exist
-mkdir -p _wiki/tools
+# Get the list of package names in the blackarch repository
+pacman -Sl blackarch | awk '{print $2}' > packages.txt
 
-# Run the pacman command and save the output to a variable
-pkgs=$(pacman -Sgg | grep blackarch)
+# Create the tools.md file with the path /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/
+mkdir -p /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools
+touch /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools.md
 
-# Create the tools.md file if it doesn't exist
-touch _wiki/tools.md
-echo "# BlackArch Packages" >> _wiki/tools.md
-
-# Loop through the packages
-while read -r pkgname pkgver pkgdesc groups url; do
-  # Create a new markdown file for each package
-  echo -e "# $pkgname\n" > _wiki/tools/$pkgname.md
-
-  # Add the package details to the file
-  echo "## Version" >> _wiki/tools/$pkgname.md
-  echo "$pkgver" >> _wiki/tools/$pkgname.md
-  echo "## Description" >> _wiki/tools/$pkgname.md
-  echo "$pkgdesc" >> _wiki/tools/$pkgname.md
-  echo "## Group" >> _wiki/tools/$pkgname.md
-  echo "$groups" >> _wiki/tools/$pkgname.md
-  echo "## URL" >> _wiki/tools/$pkgname.md
-  echo "$url" >> _wiki/tools/$pkgname.md
-  
-  # Add package name and link to package markdown file in tools.md
-  echo "- [$pkgname](tools/$pkgname.md)" >> _wiki/tools.md
-done <<< "$pkgs"
+# Iterate through the list of package names and create individual markdown files for each
+while read -r package; do
+    # Get the package details from the blackarch repository
+    package_details=$(pacman -Si $package | grep -E 'Name|URL|Description|Groups')
+    if [ $? -eq 0 ]; then
+        # Extract the package name, URL, description and group
+        package_name=$(echo $package_details | grep -oP 'Name\s*:\s*\K[^\s]*')
+        package_url=$(echo $package_details | grep -oP 'URL\s*:\s*\K[^\s]*')
+        package_desc=$(echo $package_details | grep -oP 'Description\s*:\s*\K.*' | cut -d ' ' -f2-)
+        package_group=$(echo $package_details | grep -oP 'Groups\s*:\s*\K[^\s]*')
+        # Create the individual markdown file with the package name, URL, description, and group
+        echo "---" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        echo "name: $package_name" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        echo "url: $package_url" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        echo "description: $package_desc" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        echo "group: $package_group" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        echo "---" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools/$package_name.md
+        # Add the package name to the tools.md file
+        echo "- [$package_name](/tools/$package_name)" >> /home/anunna/dev/projects/blackarch/ba-wiki/_wiki/tools.md
+        echo "Package $package_name processed successfully"
+    else
+        echo "Package $package_name failed to process"
+    fi
+done < packages.txt
