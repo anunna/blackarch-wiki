226e4f2481c2dd7ee5518852edfc6b7e21b4c9b9
Alia Morozova <anunna@lilitusec.net>
2023-01-20 21:17:42 +0200

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,12 +3,18 @@
 # Run the pacman command and save the output to a variable
 pkgs=$(pacman -Sgg | grep blackarch)
 
-# Create an empty markdown list
-echo -e "# BlackArch packages\n" > packages.md
-
 # Loop through the packages
 while read -r pkgname pkgver pkgdesc groups url; do
-  echo "* **$pkgname** - $pkgdesc" >> packages.md
-  echo "  - Version: $pkgver" >> packages.md
-  echo "  - URL: $url" >> packages.md
+  # Create a new markdown file for each package
+  echo -e "# $pkgname\n" > _wiki/tools/$pkgname.md
+
+  # Add the package details to the file
+  echo "## Version" >> _wiki/tools/$pkgname.md
+  echo "$pkgver" >> _wiki/tools/$pkgname.md
+  echo "## Description" >> _wiki/tools/$pkgname.md
+  echo "$pkgdesc" >> _wiki/tools/$pkgname.md
+  echo "## Group" >> _wiki/tools/$pkgname.md
+  echo "$groups" >> _wiki/tools/$pkgname.md
+  echo "## URL" >> _wiki/tools/$pkgname.md
+  echo "$url" >> _wiki/tools/$pkgname.md
 done <<< "$pkgs"
